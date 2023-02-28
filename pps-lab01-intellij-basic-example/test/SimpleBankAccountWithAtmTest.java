8a2c730925440e6fc98b11920b61d937f9f2ee34
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-02-28 19:15:47 +0100

    Add test initial balance is zero

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -1,7 +1,18 @@
 import lab01.example.model.AccountHolder;
 import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Backlog:
+ * - initial balance equals 0
+ * - deposit cash (withdrawing operation fee)
+ * - withdraw valid amount
+ * - withdraw invalid amount
+ */
 
 /**
  * The test suite for testing the SimpleBankAccountWithAtm implementation
@@ -11,7 +22,22 @@ public class SimpleBankAccountWithAtmTest {
     private BankAccount bankAccount;
     @BeforeEach
     void beforeEach(){
+        // Arrange
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
+    }
+
+    @Test
+    void testInitialBalanceIsZero() {
+        Assertions.assertEquals(0, bankAccount.getBalance());
+    }
+
+    @Disabled
+    void testDeposit() {
+        // Act
+        bankAccount.deposit(accountHolder.getId(), 100);
+
+        // Assert
+        Assertions.assertEquals(99, bankAccount.getBalance());
     }
 }
